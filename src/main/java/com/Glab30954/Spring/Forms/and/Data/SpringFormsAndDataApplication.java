92f214f40e6179dc36c97a9cf46ee7d0c904f3b7
batoula1 <102437354+batoula1@users.noreply.github.com>
2024-08-27 23:51:50 -0400

    Begin

---
+++ src/main/java/com/Glab30954/Spring/Forms/and/Data/SpringFormsAndDataApplication.java
@@ -0,0 +1,13 @@
+package com.Glab30954.Spring.Forms.and.Data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFormsAndDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFormsAndDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Glab30954/Spring/Forms/and/Data/SpringFormsAndDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Glab30954.Spring.Forms.and.Data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFormsAndDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
